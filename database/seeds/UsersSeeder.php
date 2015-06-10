7ed6553c5bb4479d38f06e88722c60d11cf0c669
Matthias Langhard <matthias@langaust.com>
2015-06-10 08:35:17 +0200

    changes mensurs pwd for testing reasons

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -20,7 +20,7 @@ class UsersSeeder extends Seeder {
 
         $users = array(
             ['id' => 1, 'name' => 'Matthias', 'email' => 'risc@langaust.com', 'password' => '$2y$10$kpkDskWOWuvBYbPI8pD46ORpcoC4.bRZFUM0m4uMI4zOFIINxyQFK', 'remember_token' => 'CFDloegyia6ELAqp56fbkt6Xkrhhk3sCCpnRdpMW0iVGcWE77tCBhuyz4k4f', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-            ['id' => 2, 'name' => 'Mensur', 'email' => 'mensur@mensur.com', 'password' => '$2y$10$kpkDskWOWuvBYbPI8pD46ORpcoC4.bRZFUM0m4uMI4zOFIINxyQFK', 'remember_token' => '', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['id' => 2, 'name' => 'Mensur', 'email' => 'mensur.gafuri@stanzwerk.com', 'password' => '$2y$10$dPF.rZyJKJy9JaFFuzet3ukx7rUvlTY.Z6KUizYSoTkUxWovwpJyK', 'remember_token' => '', 'created_at' => new DateTime, 'updated_at' => new DateTime],
             ['id' => 3, 'name' => 'Mike', 'email' => 'mike@mike.com', 'password' => '$2y$10$kpkDskWOWuvBYbPI8pD46ORpcoC4.bRZFUM0m4uMI4zOFIINxyQFK', 'remember_token' => '', 'created_at' => new DateTime, 'updated_at' => new DateTime],
             ['id' => 4, 'name' => 'Andy', 'email' => 'randy@andy.com', 'password' => '$2y$10$kpkDskWOWuvBYbPI8pD46ORpcoC4.bRZFUM0m4uMI4zOFIINxyQFK', 'remember_token' => '', 'created_at' => new DateTime, 'updated_at' => new DateTime],
             ['id' => 5, 'name' => 'Thomy', 'email' => 'thom@thomy.de', 'password' => '$2y$10$kpkDskWOWuvBYbPI8pD46ORpcoC4.bRZFUM0m4uMI4zOFIINxyQFK', 'remember_token' => '', 'created_at' => new DateTime, 'updated_at' => new DateTime],
