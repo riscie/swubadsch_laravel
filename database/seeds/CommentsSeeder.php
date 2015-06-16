0fc5af03d5ca9653b7d7d06da688b98e03c35340
riscie <matthias@langaust.com>
2015-06-16 12:14:58 +0200

    changed faulty comment in seeder

--- database/seeds/CommentsSeeder.php
+++ database/seeds/CommentsSeeder.php
@@ -19,7 +19,7 @@ class CommentsSeeder extends Seeder {
             ['user_id' => '1', 'date_id' => 2, 'text' => 'Spielt heute sonst noch wer?', 'created_at' => new DateTime, 'updated_at' => new DateTime],
             ['user_id' => '1', 'date_id' => 3, 'text' => 'Wer fährt? Ich bin morgen mit dem Zug da...', 'created_at' => new DateTime, 'updated_at' => new DateTime],
             ['user_id' => '3', 'date_id' => 3, 'text' => 'Ich fahre!', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-            ['user_id' => '2', 'date_id' => 7, 'text' => 'Freitgas-Sport! Who\'s in?', 'created_at' => new DateTime, 'updated_at' => new DateTime]
+            ['user_id' => '2', 'date_id' => 7, 'text' => 'Who\'s in?', 'created_at' => new DateTime, 'updated_at' => new DateTime]
         );
 
         // Uncomment to run the seeder
