a54622c34e2c6906ba01f02c6a8afb2d3d0a6883
Riscie <matthias@langaust.com>
2015-07-10 18:34:24 +0100

    fixed avatar problem

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,11 +37,9 @@
 				</button>
 				<a class="navbar-brand" href="/"><strong>SWU </strong> <strong>Bad</strong>minton <strong>Sch</strong>eduler</a>
 			</div>
-
+			
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
                 <ul class="nav navbar-nav navbar-right">
-
                     @include('flash::message')
                     @if ($errors->any())
                         <li class="active flashMessage" id="flasher"><a id="transparent"><span class="alert alert-danger">
@@ -53,7 +51,12 @@
 						<li><a href="{{ url('/auth/register') }}">Register</a></li>
 					@else
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><img class="avatar-frame-navbar" width="30" src="{{ asset('/avatarImages/unknown.jpg') }}"> {{ Auth::user()->name }} <span class="caret"></span></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+								@if(Auth::user()->avatar)
+									<img class="avatar-frame-navbar" width="30" src="{{ asset('/avatarImages/'.Auth::user()->avatar->filename) }}"> {{ Auth::user()->name }} <span class="caret"></span></a>
+								@else
+									<img class="avatar-frame-navbar" width="30" src="{{ asset('/avatarImages/unknown.jpg') }}"> {{ Auth::user()->name }} <span class="caret"></span></a>
+								@endif
 							<ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/avatars') }}">Avatar</a></li>
                                 <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
