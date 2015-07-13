d7558a99521a203bfadbc5d45f9ded3e43c7e5c8
U-SWU\lh <lh@NB2737.swu.stanzwerk.ch>
2015-07-13 11:01:24 +0200

    deleted test.php

--- resources/views/avatars/index.blade.php
+++ resources/views/avatars/index.blade.php
@@ -6,5 +6,9 @@
                     <a href="/avatars/{{$avatar->basename}}"><img height="64" src="{{ asset('/avatarImages/'.$avatar->basename) }}" class="avatar"></a>
                 @endforeach
 
+
+<h3>Upload your own Avatar</h3>
+{!! Form::file('image') !!}
+
     <br><br><b><a href="/">Zurück</a></b>
 @endsection
